a91ce9f87d67525cb07906fb7e524898274bf139
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-02 13:29:58 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,20 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
+  # def artist_name
+  #   binding.pry
+  #   self.artist ? self.artist.name : nil
+  # end
+
+  # def artist_name=(name)
+  #   self.artist = Artist.find_or_create_by(name: name)
+  # end
   def artist_name
+    artist.name if artist
   end
 
   def artist_name=(name)
+    a = Artist.find_or_create_by(name: name)
+    self.artist = a
   end
 end
